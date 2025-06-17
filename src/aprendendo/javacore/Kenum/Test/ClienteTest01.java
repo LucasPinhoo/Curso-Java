f49b250164130ce1114368ff025d859e9b907b94
LucasPinhoo <lucaspinhofelix18@gmail.com>
2025-06-17 14:28:33 -0300

    ...

--- src/aprendendo/javacore/Kenum/Test/ClienteTest01.java
+++ src/aprendendo/javacore/Kenum/Test/ClienteTest01.java
@@ -2,17 +2,24 @@ package aprendendo.javacore.Kenum.Test;
 
 import aprendendo.javacore.Kenum.Domain.Cliente;
 import aprendendo.javacore.Kenum.Domain.TipoCliente;
+import aprendendo.javacore.Kenum.Domain.TipoPagamento;
+
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Vincent", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Vincent", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Vincent", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Vincent", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Vincent", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Sephiroth", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+        TipoCliente tipoCliente = TipoCliente.valueOf("PESSOA_FISICA");
+        System.out.println(tipoCliente.getNomeRelatorio());
+        TipoCliente tipoCliente2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoCliente2);
+
+
     }
 }
--- src/aprendendo/javacore/Lclassesabstratas/Test/FuncionarioTest01.java
+++ src/aprendendo/javacore/Lclassesabstratas/Test/FuncionarioTest01.java
@@ -7,7 +7,7 @@ import aprendendo.javacore.Lclassesabstratas.Domain.Gerente;
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Gerente gerente = new Gerente("Nami", 5000);
-        Desenvolvedor desenvolvedor = new Desenvolvedor("Francky", 10000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Francky", 12000);
         System.out.println(gerente);
         System.out.println(desenvolvedor);
     }
