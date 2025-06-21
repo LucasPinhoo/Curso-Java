be66b808a613ca03c19b25465bf6c65fcf5c9152
LucasPinhoo <lucaspinhofelix18@gmail.com>
2025-06-21 20:00:55 -0300

    Parâmetros polimórficos

--- src/aprendendo/javacore/Npolimorfismo/Test/ProdutoTest01.java
+++ src/aprendendo/javacore/Npolimorfismo/Test/ProdutoTest01.java
@@ -1,14 +1,19 @@
 package aprendendo.javacore.Npolimorfismo.Test;
 
 import aprendendo.javacore.Npolimorfismo.Domain.Computador;
+import aprendendo.javacore.Npolimorfismo.Domain.Televisao;
 import aprendendo.javacore.Npolimorfismo.Domain.Tomate;
+import aprendendo.javacore.Npolimorfismo.Service.CalculadoraImposto;
 
 public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 11000);
         Tomate tomate = new Tomate("Tomate Cereja", 10);
-        aprendendo.javacore.Npolimorfismo.Service.CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
+        aprendendo.javacore.Npolimorfismo.Service.CalculadoraImposto.calcularImposto(computador);
         System.out.println("-----------------------------------------");
-        aprendendo.javacore.Npolimorfismo.Service.CalculadoraImposto.calcularImpostoTomate(tomate);
+        aprendendo.javacore.Npolimorfismo.Service.CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("-----------------------------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
